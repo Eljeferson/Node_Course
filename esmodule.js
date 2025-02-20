d6f75340a30ea51068fb1e6b32ef0c762bd2dd6d
unknown <Administrador@DESKTOP-NB79ILQ.sedamhuancayo.com.pe>
2025-02-20 09:27:58 -0500

    Actualizacion Jueves-20-02

---
+++ esmodule.js
@@ -0,0 +1,4 @@
+import math from './math/index.js'
+// en vez de math se le pone {} y en adrento aparecera las funciones que se estan importando
+math.add(10,20)
+console.log(math.add(10,20))
--- math/index.js
+++ math/index.js
@@ -1,7 +1,7 @@
 function add(x,y){
     return x+y;
 }
-
+// se puede importar desd 1 hasta todos los modulos  se agregar " export" antes de ña function 
 function subtract(x,y){
     return x-y;
 }
@@ -13,8 +13,10 @@ function multiply(x,y){
 function divide(x,y){
     return x/y;
 }
-
-module.exports = {
+// module.exports = --> esto se puede usar en vez de exoirt default
+// se usa lo de abajo por ser mas moderna
+// para poder usar eso se debe agregar  "type" : "module", en el package.json  
+export default  {
     add,
     subtract,
     multiply,
